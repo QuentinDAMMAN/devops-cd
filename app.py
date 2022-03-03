a3ac89aabd42c7a80a45902406602dbc5ea9f5ac
Quentin DAMMAN <quentindamman59@gmail.com>
2022-03-03 16:51:50 +0100

    docs: documentation du fichier principal

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 import os
 
+# Récuperation des ENVs
 PORT = os.environ.get("PORT", "80")
 HOST = os.environ.get("HOST", "0.0.0.0")
 
@@ -8,6 +9,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    """Fonction appelé pour le chemin par defaut
+
+    Returns:
+        str: un message de la plus grande importance
+    """
     return 'Hello, World !'
 
 if __name__ == "__maim__":
